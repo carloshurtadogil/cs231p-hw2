9d09cf83ed42b5a97ca6a2c3c4bdc8e08ddefdf1
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-10-19 17:22:40 -0700

    fixed spacing in main

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 int main(int argc, char **argv){
     //help message
     if(argc != 5){
-	printf("USAGE: %s max met t verb\n",argv[0]);
-	printf("max     : Maximum number to scan primes on.\n");
-	printf("met     : Method: Single thread = 's'\n");
-	printf("                  Multi thread  = 'm'\n");
-	printf("t       : Number of threads to use\n");
-	printf("          Ignored when met=s\n");
-	printf("verb    : 0=prints only execution time.\n");
-	printf("          1=prints also the numbers.\n");
-	exit(1);
+			printf("USAGE: %s max met t verb\n",argv[0]);
+			printf("max     : Maximum number to scan primes on.\n");
+			printf("met     : Method: Single thread = 's'\n");
+			printf("                  Multi thread  = 'm'\n");
+			printf("t       : Number of threads to use\n");
+			printf("          Ignored when met=s\n");
+			printf("verb    : 0=prints only execution time.\n");
+			printf("          1=prints also the numbers.\n");
+			exit(1);
     }
 
     //get the command line arguments
@@ -23,30 +23,30 @@ int main(int argc, char **argv){
     met=*argv[2];
     t=atoi(argv[3]);
     verb=atoi(argv[4]);
-    if(10000000 < max){
-	printf("You can search primes up ");
-	printf("to 10^7.\n");
-	exit(1);
+    if(10000000 < max) {
+			printf("You can search primes up ");
+			printf("to 10^7.\n");
+			exit(1);
     }
-    if(met == 'm' && t < 1){
-	printf("The number of threads 't' (%u) must ", t);
-	printf("be >= 1.\n");
-	exit(1);
+    if(met == 'm' && t < 1) {
+			printf("The number of threads 't' (%u) must ", t);
+			printf("be >= 1.\n");
+			exit(1);
     }
 
     //run and measure time taken
     double end, start;
     if(met == 's'){
-	start = stopwatch();
-	primes_st(max, verb);
-	end = stopwatch();
-    }else if(met == 'm'){
-	start = stopwatch();
-	primes_mt(max, t, verb);
-	end = stopwatch();
-    }else{
-	printf("Error: incorrect value of 'met' (%c).\n", met);
-	exit(1);
+			start = stopwatch();
+			primes_st(max, verb);
+			end = stopwatch();
+    } else if(met == 'm'){
+			start = stopwatch();
+			primes_mt(max, t, verb);
+			end = stopwatch();
+    } else {
+			printf("Error: incorrect value of 'met' (%c).\n", met);
+			exit(1);
     }
 
     //print execution time
