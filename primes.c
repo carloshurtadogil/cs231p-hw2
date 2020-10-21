b2747a2431d5dda67b7f9e0aecb41f3d914c280d
Carlos Hurtaado <carlos.hurtado19@yahoo.com>
2020-10-21 11:09:43 -0700

    documentation

--- primes.c
+++ primes.c
@@ -5,16 +5,20 @@
 
 #include "util.h" //implementing
 
-#define BATCH_SIZE 50
+#define BATCH_SIZE 50 // max amount of candidate numbers a pthread can read
 
-unsigned int current_candidate_prime;
-pthread_mutex_t lock;
-unsigned int max_num;
-bool verbosity;
+/* GLOBAL VARIABLES */
+unsigned int current_candidate_prime; // number to be tested
+pthread_mutex_t lock; // lock status used to properly assign candidate primes
+unsigned int max_num; // maximum number of candidates to evaluate
+bool verbosity; // flag to determine whether to print primalities
+
+/* FUNCTIONS */
+
 /**
  * Checks if the given number is prime using a naive approach.
  * @param[in] num The number of which to check the primality.
- * @return[out] Its primality.
+ * @return[out] Its primality in a form of a boolean.
  */
 static bool is_prime(unsigned int num)
 {
@@ -31,13 +35,6 @@ static bool is_prime(unsigned int num)
         i += 6;
     }
     return true;
-}
-
-void primes_st(unsigned int max, unsigned int verb)
-{
-    for (int i = 0; i < max; ++i)
-        if (is_prime(i) && verb != 0)
-            printf("%d\n", i);
 }
 
 /**
@@ -90,6 +87,24 @@ void *consume_and_test(void *_)
     return NULL;
 }
 
+/**
+ * Sequentially test primes using a single thread
+ * @param[in] max The maximum amount of primes to check
+ * @param[in] verb flag to determine whether to print primalities
+ */
+void primes_st(unsigned int max, unsigned int verb)
+{
+    for (int i = 0; i < max; ++i)
+        if (is_prime(i) && verb != 0)
+            printf("%d\n", i);
+}
+
+/**
+ * Sequentially test primes across multiple threads
+ * @param[in] max The maximum amount of primes to check
+ * @param[in] threads The amount of threads allowed
+ * @param[in] verb flag to determine whether to print primalities
+ */
 void primes_mt(unsigned int max, unsigned int threads,
                unsigned int verb)
 {
